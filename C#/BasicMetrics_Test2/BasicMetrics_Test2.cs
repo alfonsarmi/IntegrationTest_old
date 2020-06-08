de26209a4794553a39f1138b79eb1cfdf704c937
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-06-08 11:55:12 +0200

    c# BasicMetric Tests

---
+++ C#/BasicMetrics_Test2/BasicMetrics_Test2.cs
@@ -0,0 +1,52 @@
+﻿namespace BasicMetrics_Test
+{
+    class BasicMetricsTest2
+    {
+        //This is a comment in class
+        private void BasicMetricsTest_Method(int a)
+        {
+            //This is a comment in method
+            for (int i = 0; i < 10; i++)
+            {
+                for (int u = 0; u < 10; u++)
+                {
+                    for (int y = 0; y < 10; y++)
+                    {
+                        var v = 0;
+                        v = 1 + 1;
+                        //this is super nested comment
+                    }
+                }
+            }
+
+            var z = 0;
+            while (z < 10)
+            {
+                z++;
+            }
+        }
+
+        private void BasicMetricsTest_Method(long a)
+        {
+            //This is a comment in method
+            for (int i = 0; i < 10; i++)
+            {
+                for (int u = 0; u < 10; u++)
+                {
+                    for (int y = 0; y < 10; y++)
+                    {
+                        var v = 0;
+                        v = 1 + 1;
+                        //this is super nested comment
+                    }
+                }
+            }
+
+            var z = 0;
+            while (z < 10)
+            {
+                z++;
+            }
+        }
+    }
+}
